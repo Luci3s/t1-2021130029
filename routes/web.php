33c8f28f24258dd130e29376117c76491c8ff985
yonas <yonayehezkiel@gmail.com>
2023-10-14 11:30:49 +0700

    Repopulate Form

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,33 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+    return view('landing');
+});
+
+Route::view('/', 'landing');
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/about', function () {
+    $title = 'About Us';
+    $description = 'Blogging is website for sharing your thoughts and ideas with the world.';
+    $button = '<a class="btn btn-lg btn-secondary" href="/">Back to Landing Page</a>';
+    return view('about', compact('title', 'description', 'button'));
+});
+
+use App\Http\Controllers\AboutController;
+
+Route::get('/about', [AboutController::class, 'index']);
+
+use App\Http\Controllers\LandingController;
+
+Route::get('/', LandingController::class);
+
+use App\Http\Controllers\ContactController;
+
+Route::get('/contact-us', [ContactController::class, 'index']);
+Route::post('/contact-us', [ContactController::class, 'store']);
